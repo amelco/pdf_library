3179074fcff8ba00b6063746b08d23945389ade2
Andre Herman <amelco.herman@gmail.com>
2015-02-26 17:26:45 -0300

    Modified to execute at each pdf file of the current directory

--- create_lib.sh
+++ create_lib.sh
@@ -2,33 +2,43 @@
 # User oriented program that sweeps all pdf files of a directory and
 # makes OCR, asking for the proper variables values.
 
+### Initialy, put the script into the directory of the pdfs
+#if [ $# -eq 0 ]; then
+#  # No arguments supplied
+#  echo "Type the path of the directory containing the PDFs you want to put in the database:"
+#  read -r dir
+#else
+#  # First argument is the directory
+#  dir=$1
+#fi
+#
 
-if [ $# -eq 0 ]; then
-  # No arguments supplied
-  echo "Type the path of the directory containing the PDFs you want to put in the database:"
-  read -r dir
-else
-  # First argument is the directory
-  dir=$1
-fi
+# creates a txt file with all pdf filenames (full extension)
+ls *.pdf > filenames.txt
+#asks for last number of pdf file of the original database
+echo "What is the last number of the database?"
+read -r id
+#creates a new directory to store the renamed pdf files
+new_dir='renamed_pdfs'
+mkdir $new_dir
 
-
-filename='cushman_1979.pdf'		# name of the file to be OCRed
-filename='s06.pdf'		# name of the file to be OCRed
-filename='[Daniel_Hillel_(Auth.)]_Applications_of_Soil_Physi(BookZZ.org).pdf'		# name of the file to be OCRed
-filename='10.pdf'		# name of the file to be OCRed
-				# key pressed
-
+OLDIFS=$IFS
+IFS=$'\n'
+for line in $(cat filenames.txt)
+do
+#Beginning of the loop #############################################################
 # OCr the file to a temporary txt file
-pdftotext ${filename} tmp.txt
+pdftotext ${line} tmp.txt
+
 tot_lines=$(cat tmp.txt | wc -l)	# total of lines in the OCRed file
 li=0					# initial line
-inc=50					# increment
+inc=40					# increment
 le=$inc					# end line
-key="n"	
-echo
-echo "#lines: ${tot_lines}"
-read
+key="n"					# key pressed
+
+#echo
+#echo "#lines: ${tot_lines}"
+#read
 
 # scans the file at each $inc lines
 while [ $li -le "$((tot_lines))" ]; do
@@ -82,13 +92,16 @@ echo $author
 echo $year
 echo $type
 
-# Verificar o ultimo numero do arquivo de dados
-id='101'
+# Verificar o ultimo numero do arquivo de dados - now done with user inpit at the beginning of the script
+#id='101'
 # Adicionar um novo registro no arquivo de dados
-# renomear o PDF
+# copia PDF para novo diretorio, mantendo o PDF original
+id=$id+1
 newfilename='0101.pdf'
 
 echo
 echo "$id;$newfilename;$type;$title;$author;$year"
-
+############################################################################
+done
+IFS=$OLDIFS
 #rm tmp.txt
