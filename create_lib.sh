b603fc05389bfbdf0d9f33b698e9ef0f29e4a806
Andre Herman <amelco.herman@gmail.com>
2015-03-09 17:47:55 -0300

    Added arguments It is only possible to execute the script when givvinh two arguments. The first one is the location of the PDF files to be converted. The second is the name of the database file (.dat) A bug must be corrected: it doesn't change the select database file. It creates a new file with the same name of the database inside the PDFs directory. One idea to solve this: check where the database directory is. A new variable might be created to take this into account.

--- create_lib.sh
+++ create_lib.sh
@@ -11,6 +11,11 @@
 # * a temporary txt file (filenames.txt) is created with the names of the original pdf files. The script deletes this file in the end
 # * a temporary txt file (tmp.txt) containing the OCRed pdf text is created. The script deletes this file in the end
 #
+### Modifications
+# 09/03/2015:	* Added first argument as the directory for the PDF files
+#		* Second argument is the database txt file that wants to add the new PDF files
+#		* Added confirmation of the input arguments. If it is not correct, exits the script
+#
 ##########################################################################################################################################
 #
 # Developed by Andre Herman
@@ -22,36 +27,96 @@
 
 
 ## For further version: the directory that contains the pdf files is asked if no argument is given
-#if [ $# -eq 0 ]; then
-#  # No arguments supplied
-#  echo "Type the path of the directory containing the PDFs you want to put in the database:"
-#  read -r dir
-#else
-#  # First argument is the directory
-#  dir=$1
-#fi
-#
+if [ $# -lt 2 ]; then
+  # No arguments supplied
+  #echo "Type the path of the directory containing the PDFs you want to put in the database:"
+  #read -r dir
+  echo "No arguments were provided. Please give the following arguments:"
+  echo "1 - relative or absolute directory of the PDF files that you want to add in the database"
+  echo "2 - database text file (.dat) that you want to add the new entries"
+  echo
+  echo "Example: $0 /home/user/papers my_papers.dat"
+  echo
+  echo "Exiting script"
+  echo
+  exit
+else
+  # First argument is the directory
+  rel_dir=$1
+  # Second argument is the database file
+  DB=$2
+  # First char of the first argument
+  fst_char=${rel_dir:0:1}
+  lst_num=`awk -F ';' 'END{print $1}' $DB`
+  # check if the directory is relative and changes it to absolute
+  if [ $fst_char == "/" ]; then
+    dir=$rel_dir
+  else
+    dir=`pwd`"/$rel_dir"
+  fi
+  # get length of $dir
+  len=${#dir}
+  # Last char of absolute directory
+  # if it is not a '/', add it
+  lst_char=${dir:$((len-1)):1}
+  if [ $lst_char != "/" ]; then
+    dir="$dir/"
+  fi
+fi
+
 
 #### Parameters that can be changed by the user ###################################
 # new database filename
 newDBfilename='newDB.txt'
 # new directory for the renamed pdf files
-newdir='renamed_pdfs'
+newdir=$dir"renamed_pdfs"
 # number of lines of the current pdf file to be shown on the screen at once
 inc=40
 ###################################################################################
 
 
 
-#### Beginning of the process. User can not change anything here##################
 
+#### Beginning of the process. User can not change anything here #################
+clear
+len=${#newdir}
+lst_char=${newdir:$((len-1)):1}
+if [ $lst_char != "/" ]; then
+  newdir="$newdir/"
+fi
+
+echo
+echo "-== VERIFICATION ==-"
+echo
+printf "%-28s: %${#dir}s\n" "Path of PDF files" "$dir"
+printf "%-28s: %${#DB}s\n" "Database file" "$DB"
+printf "%-28s: %${#lst_num}s\n" "Last # in database" "$lst_num"
+printf "%-28s: %${#newdir}s\n" "Path to store renamed PDFs" "$newdir"
+echo 
+echo "==============="
+echo
+echo "Is this information correct? [y/N]"
+read -r -n1 iniParam
+if [ "$iniParam" = "n" ] || [ "$key" = "N" ]; then
+  echo
+  echo "Execute the script again with the correct arguments. Type $0 for a description"
+  echo
+  echo "Exiting script"
+  exit
+fi
+
+#change to the directory where the PDFs are
+cd $dir
+#doing backup of the original database
+cp $DB $DB".bkp"
 #cleaning and creating the newdatabase file
 echo "" > $newDBfilename
 # creates a txt file with all pdf filenames (full extension)
 ls *.pdf > filenames.txt
 #asks for last number of pdf file of the original database
-echo "What is the last number of the database?"
-read -r id
+#echo "What is the last number of the database?"
+#read -r id
+id=$lst_num
 #creates a new directory to store the renamed pdf files
 mkdir $newdir
 
@@ -69,8 +134,8 @@ for line in $(cat filenames.txt)
   
   tot_lines=$(cat tmp.txt | wc -l)	# total of lines in the OCRed file
   li=0					# initial line
-  le=$inc					# end line
-  key="n"					# key pressed ('n' is default => [n]ext)
+  le=$inc				# end line
+  key="n"				# key pressed ('n' is default => [n]ext)
   
   #echo
   #echo "#lines: ${tot_lines}"
@@ -127,33 +192,29 @@ for line in $(cat filenames.txt)
   echo $year
   echo $type
   
-  # Further version: verify the last number of datafile (now, this is done with user input at the beginning of the script)
-  #id='101'
-  # Adicionar um novo registro no arquivo de dados
-  # copia PDF para novo diretorio, mantendo o PDF original
+  # Increments the file id and atributes the new file name
   id=$((id+1))
   newfilename="$id.pdf"
   
-  # Shows the new register that will be added in the new database file
+  # Shows the new register that will be added in the new database file, and add it
   echo
   echo "$id;$newfilename;$type;$title;$author;$year"
   echo "$id;$newfilename;$type;$title;$author;$year" >> $newDBfilename
+  echo "$id;$newfilename;$type;$title;$author;$year" >> $DB
   # Copy and renames the pdf file to the new directory
   cp $line "$newdir/$newfilename"
   
   echo
-  echo "$line has been renamed to $newfilename and copied to $newdir/"
-  echo
-  echo "OCRing the next file... please wait"
+  echo "$line has been renamed to $newfilename and copied to $newdir"
 done
 ### End of loop ##########
 IFS=$OLDIFS
 
-cp "$newDBfilename $newdir/."
+cp "$newDBfilename $newdir""."
 rm filenames.txt
 rm tmp.txt
 echo
-echo "Database file $newDBfilename has been copied to $newdir/"
+echo "Database file $newDBfilename has been copied to $newdir"
 echo
 echo "Done!"
 echo
