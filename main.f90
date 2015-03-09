12a1ed2ffe6c256cdc7783c286ee642b68387ce4
Andre Herman <amelco.herman@gmail.com>
2015-03-09 18:49:33 -0300

    Added a new option. [4] Create BiBTeX file With this option, the user can create a new .bib file with any PDF that it has in the database. The user just need to type the number(s) of the PDF(s) file(s) and the script for look for BiB entries in the internet takes place. It is a difficult task to automatize the bib search script. For now, it asks to the user which entry is the correct.

--- main.f90
+++ main.f90
@@ -24,6 +24,8 @@ do while (opt .ne. "Q" .and. opt .ne. "q")
       call search_inside()
     case("3")
       call bib_search()
+    case("4")
+      call bib_create()
     case("B","b")
       call choose_DB()
     case("C", "c")
@@ -52,6 +54,7 @@ print*, " Choose an option:"
 print*, " [1] Search by author or title"
 print*, " [2] Search string inside text"
 print*, " [3] Make a BiBTex search"
+print*, " [4] Create a BiBTex file (soon)"
 print*
 print*, " [B] Choose database"
 print*, " [C] View/Change settings"
@@ -126,13 +129,20 @@ implicit none
       ! quit to menu
       case("Q","q")
         print*
+        read(*,*) slc
+
+      ! add to bib file
+      !case("+")
+      !  print*, "Added to bibfile" 
+      !  read(*,*) slc
 
       ! open the file
       case default
 	! -- In this command, the id must be the same as the filename.
 	! -- To improve the code, a search for the right file with the given id must be made.
         !cmd = 'evince '//trim(adjustl(path))//'`cat '//datafile//' | grep -i '''//trim(adjustl(slc))//';'' | awk ''BEGIN{FS=";"}{print $2}'' | head -1`'
-        cmd = reader//' '//trim(adjustl(path))//'`cat '//datafile//' | grep -i '''//trim(adjustl(slc))//';'' | awk ''BEGIN{FS=";"}{print $2}'' | head -1`'//"& > /dev/null 2>&1"
+        cmd = reader//' '//trim(adjustl(path))//'`cat '//datafile//' | grep -i '''// &
+           trim(adjustl(slc))//';'' | awk ''BEGIN{FS=";"}{print $2}'' | head -1`'//"& > /dev/null 2>&1"
 	call system(cmd)
 	slc = "I"
     end select
@@ -150,7 +160,8 @@ implicit none
   print*, "'Space key' to scrool several lines at once;"
   print*, "To quit the results list, hit 'q'."
   print*
-  print*, "Put an '*' at the end of the string to show all results that contains the string that you want. Ex.: solute* will show either solute, absolute, absolutely..."
+  print*, "Put an '*' at the end of the string to show all results that &
+     contains the string that you want. Ex.: solute* will show either solute, absolute, absolutely..."
   print*
   print*, "Type the search string:" 
   read(*,'(A)') str
@@ -160,9 +171,13 @@ implicit none
 
   if (index(str,'*') > 0) then
     str = str(1:index(str,'*')-1)
-    cmd = "find "//trim(adjustl(path))//" -name '*.pdf' -exec sh -c 'pdftotext ""{}"" - | grep -i --with-filename --label=""{}"" --color=always """//trim(adjustl(str))//"""' \; | less -r 2>/dev/null"
+    cmd = "find "//trim(adjustl(path))// &
+      " -name '*.pdf' -exec sh -c 'pdftotext ""{}"" - | grep -i --with-filename --label=""{}"" --color=always """// &
+      trim(adjustl(str))//"""' \; | less -r 2>/dev/null"
   else
-    cmd = "find "//trim(adjustl(path))//" -name '*.pdf' -exec sh -c 'pdftotext ""{}"" - | grep -i -w --with-filename --label=""{}"" --color=always """//trim(adjustl(str))//"""' \; | less -r 2>/dev/null"
+    cmd = "find "//trim(adjustl(path))// &
+      " -name '*.pdf' -exec sh -c 'pdftotext ""{}"" - | grep -i -w --with-filename --label=""{}"" --color=always """// &
+      trim(adjustl(str))//"""' \; | less -r 2>/dev/null"
   endif
   print*, cmd
   call system(cmd)
@@ -184,7 +199,8 @@ implicit none
   write(*,'(A)',advance='no') '   => '
   read(*,*) slc
 !  cmd = 'sed ''1 c\ `ls -l *.dat | awk ''BEGIN{i=1} {print "  ["i"]",$9; i++}'' | awk ''NR=='//slc//' {print $2}''` settings.in'
-  cmd = "sed -i '1s/.*/'""`ls -l *.dat | awk 'BEGIN{i=1} {print ""[""i""]"",$9; i++}' | awk 'NR=="//slc//" {print ""\042""$2""\042""}'`""'/' settings.in" 
+  cmd = "sed -i '1s/.*/'""`ls -l *.dat | awk 'BEGIN{i=1} {print ""[""i""]"",$9; i++}' | awk 'NR=="// &
+    slc//" {print ""\042""$2""\042""}'`""'/' settings.in" 
   call system(cmd)
   call read_settings()
   print*
@@ -228,6 +244,22 @@ implicit none
   print*
   write(*,*) 'The BibTex reference has been saved in ''library.bib'' file.'
   read(*,*)
+end subroutine
+
+subroutine bib_create()
+implicit none
+  character(500) :: cmd1,cmd2,cmd
+
+  call system("clear")
+  cmd1 = 'cat '//datafile
+  cmd2 = ' | awk ''BEGIN{FS=";"}{printf "[%4s]%-10s%-7s%-81.81s %-4s %-52.52s\n",  $1,$2,$3,$4,$6,$5}'''
+  cmd = trim(adjustl(cmd1)) // ' | sort -t '';'' -nbk1' //  trim(adjustl(cmd2))
+  call system(cmd)
+  print*
+  print*, "Type the file number(s) that you want to add as a BiBTeX entry in the "// &
+    "bib file"
+  read(*,*)
+
 end subroutine
 
 subroutine show_settings()
